f06602eb3df480c1392d7a645dfbec7b30050f2d
kaboria <kaporyakak@gmail.com>
2024-07-29 16:08:22 +0300

    lesson 3

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3001;
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.send('./app.js')
-})
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
 
-app.listen(port, () => {
-  console.log(`http://localhost:3000/`)
-})
+
+
+mongoose
+  .connect(
+    "mongodb+srv://kaporya:nikenike123@cluster0.5lrfscm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
